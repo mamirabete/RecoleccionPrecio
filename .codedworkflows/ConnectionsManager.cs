befc20907ffb8068fb9be813fe9e80c4116f9c9f
Martín Mirabete <UNPA.martinmirabete@gmail.com>
2025-11-29 11:52:15 -0300

    Se agreggo la lectura de la lista de productos inicial. Y la apertura del navegador, con las verificaciones.

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace RecoleccionCBA
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
